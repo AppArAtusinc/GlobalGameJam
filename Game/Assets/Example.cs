7def61e78a18be7419903ba42d188865ccb1544d
Admin <EdwardMuzhevskiy@gmail.com>
2017-01-20 23:09:54 +0200

    music

---
+++ Game/Assets/Example.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class Example : MonoBehaviour, AudioProcessor.AudioCallbacks
+{
+    void Start()
+    {
+        //Select the instance of AudioProcessor and pass a reference
+        //to this object
+        AudioProcessor processor = FindObjectOfType<AudioProcessor>();
+        processor.addAudioCallback(this);
+    }
+
+
+    void Update()
+    {
+
+    }
+
+    public void onOnbeatDetected()
+    {
+        Debug.Log("Beat!!!");
+    }
+
+    public void onSpectrum(float[] spectrum)
+    {
+
+    }
+}
