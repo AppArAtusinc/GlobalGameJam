42c3990e3a787452e79bac00230f935b73899339
Stanislav Silin <byme.work@gmail.com>
2017-01-21 21:10:11 +0200

    Add hexes and start on floor ganeration

---
+++ Game/Assets/FloorGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorGenerator : MonoBehaviour
+{
+	public int Width;
+	public int Height;
+	public GameObject HexTemplate;
+
+	private void Start()
+	{
+		const float HorizontalShift = 1;
+		const float VerticalShift = 1;
+		var transform = this.transform;
+		for (int i = 0; i < this.Width; i++)
+			for (int j = 0; j < this.Height; j++)
+				this.HexTemplate.Create(new Vector3(i * HorizontalShift + i * 2, 0, j * VerticalShift), transform);
+	}
+}
