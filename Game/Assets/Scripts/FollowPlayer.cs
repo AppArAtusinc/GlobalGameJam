d854be02980a106f278b52b83ddb0ceb2af45816
Admin <EdwardMuzhevskiy@gmail.com>
2017-01-21 19:27:28 +0200

    cleanup

---
+++ Game/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour {
+
+	Transform TR, playerTR;
+
+	public float speed = 10;
+
+	private void Awake()
+	{
+		TR = transform;
+		playerTR = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		float step = speed * Time.deltaTime;
+		transform.rotation = Quaternion.RotateTowards(TR.rotation, Quaternion.LookRotation((playerTR.position-TR.position).normalized,TR.up), step);
+	}
+}
---
+++ Game/Assets/Scripts/RotateEmitter.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateEmitter : MonoBehaviour {
+
+	private void Awake()
+	{
+		transform.rotation = Quaternion.Euler(new Vector3(90, -GetComponent<ParticleSystem>().shape.arc / 2, 0));
+	}
+}
