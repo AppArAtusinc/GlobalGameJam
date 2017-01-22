236f5cb3f6f34e51f7fed540799e42c572972fdd
Stanislav Silin <byme.work@gmail.com>
2017-01-22 08:50:27 +0200

    Add pain sounds.

--- Game/Assets/Scripts/PlayerHealth.cs
+++ Game/Assets/Scripts/PlayerHealth.cs
@@ -2,24 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerHealth : Health {
+public class PlayerHealth : Health
+{
+	public AudioClip[] Pain;
+	private AudioSource AudioSource;
 
-    protected override void OnEnable()
-    {
-        base.OnEnable();
-        UiManager.instance.SetHealthLevel(health / MAX_HEALTH);
-    }
+	protected override void OnEnable()
+	{
+		base.OnEnable();
+		UiManager.instance.SetHealthLevel(health / MAX_HEALTH);
+	}
 
-    public override void Damage(float damage)
-    {
-        base.Damage(damage);
-        UiManager.instance.SetHealthLevel(health / MAX_HEALTH);
-    }
+	private void Start()
+	{
+		this.AudioSource = this.GetComponent<AudioSource>();
+	}
 
-    public override void Die()
-    {
-        GetComponent<Animator>().SetTrigger("Die");
-    }
+	public override void Damage(float damage)
+	{
+		base.Damage(damage);
+		UiManager.instance.SetHealthLevel(health / MAX_HEALTH);
+
+		if (!this.AudioSource.isPlaying)
+			this.AudioSource.PlayOneShot(this.Pain.GetRandom());
+	}
+
+	public override void Die()
+	{
+		GetComponent<Animator>().SetTrigger("Die");
+	}
 
 
 
